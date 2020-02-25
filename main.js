d33e5fd92c8a8a96b702d4b14c940d931eef8bc6
crashdaddy <kennethscoggins@mail.com>
2020-02-25 15:21:23 -0600

    DOM model for 1:1 fight

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 'use strict';
+let arrayOfPokemon = [];
+
+
+const getAllPokemon = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000')
+      .then(res => res.json())
+      .then(data => {
+                
+      });
+  }
+
+  getAllPokemon();
+  
 
