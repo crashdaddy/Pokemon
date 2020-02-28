a160ffd79fa7363170370d175b6e88f18d874c56
crashdaddy <kennethscoggins@mail.com>
2020-02-28 09:18:11 -0600

    added director commentary

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 'use strict';
 
+///////////////////////////////
+//
+//  Declare global variables
+//
+
+// the id numbers of the enemy pokemons
 let arrayOfPokemon = [32,15,99,73,45,247,700,450,699,722,55,66,77,311,405,420,69,12,203,196];
-let pokemon = [];
+// the array where the enemies will be stored
+let enemies = [];
+// landscaped dimensions
 let boardHeight = 20;
 let boardWidth = 30;
+
+/////////////////////////////
+//
+// Helper functions
+//
+
+// return a random number in a range (min, max)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/////////////////////////////
+//
+// Build the class we will use to define a pokemon
+// this will be used for the enemies AND the player
+// (can be extended to add more Player specs if needed)
+
 
 class Pokemon{
    constructor (name,hp, imgURL,x, y) {
@@ -18,32 +45,46 @@ class Pokemon{
 
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+///////////////////////////////////
+//
+// API functions to get the data
+// for the enemies
+//
 
+// loads the data for an individual pokemon
+// creates the object
+// then adds it to the array of all enemies
 const getPokemonById = (id) => {
     fetch('https://pokeapi.co/api/v2/pokemon/' + id)
       .then(res => res.json())
       .then(data => {
                 let newPokemon = new Pokemon(data.name,data.stats[5].base_stat,data.sprites.front_default);
                 
-                pokemon.push(newPokemon);
-                placePokemon(pokemon.indexOf(newPokemon));
+                enemies.push(newPokemon);
+                placeEnemies(enemies.indexOf(newPokemon));
       })
       .catch((error) => {
         console.error('Error:', error + " id: " + id);
       });
   }
 
-  const getAllPokemon = () => {
+  // loops through the enemies list
+  // end gets the data for each one 
+  const getAllEnemies = () => {
       for (let i = 0; i< arrayOfPokemon.length;i++){
         getPokemonById(arrayOfPokemon[i]);
       }
   };
 
+  ////////////////////////////////
+  //
+  //  Output functions
+  //  display game elements
+  //
+
+  // build the table we'll use for our landscape
+  // the ID of each cell will match the x and y properties
+  // of each enemy and map as coordinates in the table
   const buildLandscape = () => {
     let htmlStr = `<table style="width:100%;border:1px solid black;">`;
     for (let rows = 1;rows<boardHeight+1;rows++){
@@ -57,23 +98,25 @@ const getPokemonById = (id) => {
     document.getElementById('landscape').innerHTML = htmlStr;
   }
 
-  const placePokemon = (idx) => {
-    
-      pokemon[idx].x = getRandomInt(1,boardHeight);
-      pokemon[idx].y = getRandomInt(1,boardWidth);
+// defines a random position within the landscape dimensions
+// for where each enemy will be placed
+  const placeEnemies = (idx) => {
+      enemies[idx].x = getRandomInt(1,boardHeight);
+      enemies[idx].y = getRandomInt(1,boardWidth);
       updateLocations(idx);
-    
   }
 
+  // draws each enemy to its coordinate cell in the table
   const updateLocations = (pokemonID) => {
-    let locID = pokemon[pokemonID].x.toString() + "-" + pokemon[pokemonID].y;
-    let htmlStr = `<img src="${pokemon[pokemonID].imgURL}" style="width:50px;height:50px;" title="${pokemon[pokemonID].name}">`;
+    let locID = enemies[pokemonID].x.toString() + "-" + enemies[pokemonID].y;
+    let htmlStr = `<img src="${enemies[pokemonID].imgURL}" style="width:50px;height:50px;" title="${enemies[pokemonID].name}">`;
     document.getElementById(locID).innerHTML=htmlStr;
   }
   
+  // Let's get it started in here!
   window.onload = function(){
     buildLandscape();
-    getAllPokemon();
+    getAllEnemies();
 
   }
 
